f3e8aa4dd8ce453d9fc9f496b6543e01de37f71c
krishnabagul06 <krishnabagul06@gmail.com>
2026-02-11 23:02:43 +0530

    7th Code

--- 7.cpp
+++ 7.cpp
@@ -1,2 +1,22 @@
 #include<iostream>
+#include <climits>
 using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+    int rev=0;  //This will store Reversed number
+    while(x!=0)
+    {
+        int digit=x%10;    //Take last digit
+        if(rev>INT_MAX/10 or rev<INT_MIN/10)    //Check condition before multiplying by 10
+        {
+            return 0;
+        }
+        rev=rev*10+digit;   //Add digit to reverse 
+        x=x/10;             //remove last digits
+    }
+    cout<<"Reversed number is :"<<rev;
+    return 0;
+}
